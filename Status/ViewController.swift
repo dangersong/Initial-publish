f8a20d4d4ee2bff459bec2e0675b3b4dfffa83e7
dangersong <pennq1+danger@gmail.com>
2015-06-08 19:32:36 -0700

    Set ViewController as table view data source

--- Status/ViewController.swift
+++ Status/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
      
     }
     
