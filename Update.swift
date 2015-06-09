8240dc63adf8bf71b92945c76b8d849b0a03f48c
dangersong <pennq1+danger@gmail.com>
2015-06-08 19:12:27 -0700

    Add models

---
+++ Update.swift
@@ -0,0 +1,20 @@
+//
+//  Update.swift
+//  Status
+//
+//  Created by dave on 6/8/15.
+//  Copyright (c) 2015 dave. All rights reserved.
+//
+
+import Foundation
+
+// Represents each row in a users's feed
+class Update {
+    var user: User?
+    var text: String?
+    var date: NSDate?
+    
+    
+
+    
+}
---
+++ User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Status
+//
+//  Created by dave on 6/8/15.
+//  Copyright (c) 2015 dave. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var name: String?
+    var bio: String?
+    var username: String?
+    var link: String?
+    var city: String?
+    
+}
