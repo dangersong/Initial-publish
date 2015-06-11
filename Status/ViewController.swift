608c0722eaedaf0d11c174a9cdf29e5db91d69b9
dangersong <pennq1+danger@gmail.com>
2015-06-11 01:43:38 -0700

    Revert 1ac7d66..0987ef3
    This rolls back to commit 1ac7d664fc700433c996bb1016cb6eb77a7b0aa2.

--- Status/ViewController.swift
+++ Status/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView: UITableView!
     
     // Add array to store updates
-    var updates = [Update]()
+    var updates: [Update]?
     
     
     override func viewDidLoad() {
@@ -28,11 +28,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let request = NSURLRequest(URL: url!)
         let connection = NSURLConnection(request: request, delegate: self, startImmediately: true)
         
+        // TODO: Sample data, remove when getting real data
+        
+        // Initialize updates to a brand new array
+//        updates = [Update]()
+//        
+//        var user = User()
+//        user.username = "James"
+//        user.bio = "Him"
+//        user.city = "San Francisco"
+//        user.link = "http://somewebsite.com"
+//        
+//        for var i = 0; i < 100; i++ {
+//            var update = Update()
+//            update.date = NSDate()
+//            update.text = "Hello world! \(i)"
+//            update.user = user
+//        
+//            updates?.append(update)
+//        }
     }
     
     // MARK: - UITableViewDataSource
+    // This marker goes until the end of the of the file or another marker
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return updates.count
+        // Typically return a count of something
+        
+        // TODO: Return count of update items
+        if let updatesCount = updates?.count {
+            return updatesCount
+        }
+        
+        // Another option to test for nil
+//        if updates.count != nil {
+//            
+//        }
+
+        // Short circuiting with "return". Exits code block once return line is run.
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -44,20 +78,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 //        // Below is a bad example as we are not reusing cells
 //        var cell = UpdateTableViewCell()
         
-        var update = updates[indexPath.row]
-        cell.updateTableView?.text = update.text
         
-        if let user = update.user {
-            cell.updateUsername.text = user.username
-        }
+//         Changed this to reference our custom view, UpdateTableViewCell
+//        var cell = UITableViewCell()
         
-        if let date = update.date {
-            let dateFormatter = NSDateFormatter()
-            dateFormatter.dateFormat = "h:mm a" // superset of OP's format
-            let str = dateFormatter.stringFromDate(date)
-            cell.updateDate?.text = str
-        }
+        // Very often people will call their "if lets" as the very same thing they're looking for
+        if let updates = updates {
+            var update = updates[indexPath.row]
+            cell.updateTableView?.text = update.text
 
+            if let user = update.user {
+                cell.updateUsername.text = user.username
+            }
+
+            if let date = update.date {
+                let dateFormatter = NSDateFormatter()
+                dateFormatter.dateFormat = "h:mm a" // superset of OP's format
+                let str = dateFormatter.stringFromDate(date)
+                cell.updateDate?.text = str
+            }
+        }
         return cell
     }
     
@@ -78,6 +118,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
         let jsonObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSArray
             println(jsonObject)
+        
+        updates = [Update]()
         
         for var i = 0; i < jsonObject.count; i++ {
             // setting json object as a dictionary
@@ -104,48 +146,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             // TODO: convert date integer to NSDate
             update.date = NSDate()
             
-            updates.append(update)
+            updates?.append(update)
         }
         tableView.reloadData()
     }
 }
-
-
-
-
-
-
-
-
-//class SomeClass {
-//    func createPlist() {
-//        // Get a reference to the Documents Directory with User permissions
-//        // For example: /Users/james/Documents
-//        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-//        // Get the first path (only one Documents Directory)
-//        let firstPath = paths.first as! String
-//        // Add 'data.plist' onto the Documents Directory path
-//        // For example: /Users/james/Documents/data.plist
-//        let plistPath = firstPath.stringByAppendingPathComponent("data.plist")
-//        // Create a reference to the file manager
-//        let fileManager = NSFileManager.defaultManager()
-//        // Check to see if the plist exists
-//        if !fileManager.fileExistsAtPath(plistPath) {
-//            // TODO: create plist
-////        let pleaseSucceed = data.writeToFile(plistPath, atomically: true)
-//            // An array converted to an NSArray (an object)
-//            let updatesObject = updates as NSArray
-//            updatesObject.writeToFile(plistPath, atomically: true)
-//            let plistData = NSArray(contentsOfFile: plistPath)
-//        }
-//        // Write my data to the plist
-//        let myData = NSData()
-//        myData.writeToFile(plistPath, atomically: true)
-//    }
-//}
-
-
-
-
-
-
